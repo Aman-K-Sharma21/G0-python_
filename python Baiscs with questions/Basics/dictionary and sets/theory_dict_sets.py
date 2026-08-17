54b086f7b99d8be65dbbe2c6fd0b1c15c7469e6b
Aman <amankumarsharma1899@gmail.com>
2026-08-17 20:20:02 +0530

    Adding notes of dictionary and sets

---
+++ python Baiscs with questions/Basics/dictionary and sets/theory_dict_sets.py
@@ -0,0 +1,46 @@
+# # # dictionary
+
+# # # dictionary is nothing but key value pairs.
+
+# # d1 = {}
+# # print(type(d1))
+
+# # d2 = {"james":"bond" , "Allen":"Walker","Harry": "potter","Alex":{"B":"Benjamin","C":"Clark","H":"Hamilton"}}
+
+# # print(d2["Alex"]["B"]) #Benjamin
+
+# # d2["Linus"] = "Tarvold"
+# # d2["Jimmy"] = "Hargrove"
+# # d2["Stephen"] = "Hawkings"
+
+# # del d2["Stephen"] # use to delete a particular key value from the dictionary.
+# # d3 = d2.copy()
+# # d2.update({"Hermonie":"Greyjoy"})
+# # print(d2.keys())
+# # print(d2.items())
+# # print(d2.get("Linus"))
+
+# # # print(d2)
+
+# #----------------------------------------
+
+# s = set()
+# # print(type(s))
+
+# # s_from_list = set([1,2,3,4])
+# # print(s_from_list)
+# # print(type(s_from_list))
+
+# s.add(1)
+# s.add(2)
+# s1 = s.union({1,2,3})
+# # s1 = s.intersection({1,2,3})
+# print(len(s1))
+# print(type(s1))
+# print(max(s1))
+# print(min(s1))
+# print(s.isdisjoint(s1))
+# s.remove(2)
+
+# print(s,s1)
+
