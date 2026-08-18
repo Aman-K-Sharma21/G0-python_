edd78f6eba0dc9d1f050a712ed1fa774183e5368
Aman <amankumarsharma1899@gmail.com>
2026-08-18 06:31:01 +0530

    Add theory files on operators inside basics

---
+++ python Baiscs with questions/Basics/operators.py
@@ -0,0 +1,62 @@
+# operators in python
+
+# 1.Arithmetic operator
+
+print("5+6 is :", 5+6)
+print("5-6 is :", 5-6)
+print("5*6 is :", 5*6)
+print("5/6 is :", 5/6)
+print("15//6 is :", 5//6)
+print("5**3 is :", 5**3)
+print("5%3 is :", 5%3)
+
+# 2.Assignment operator
+
+x = 5
+print(x)
+x +=7 # x = x + 7
+print(x)
+
+
+# 3.Comparison operator
+
+i = 8
+print(i == 5) # False
+print(i<8) # and more
+
+# 4.Logical operator
+
+a = True
+b = False
+
+print(a or b) #or means if one condition is true then it's return true .
+print(a and b) # and means when both condition are true then it returns true
+
+
+# 5.Identity operators
+
+print(a is not b)
+print(a is not b)
+
+# 6.Membership operator
+
+# in , not in
+
+list = [3,3,2,2,45,67,88]
+print(88 not in list)
+print(45 in list)
+
+# 7.Bitwise operator
+
+# it works with binary number
+
+# 0 - 00
+# 1 - 01
+# 2 -10
+# 3 - 11
+
+# print(0 | 1)
+# print(0 & 2)
+
+
+
